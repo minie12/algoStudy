365cdda5a66a7b8932793c75a854af351d4df9d9
minie12 <30361753+minie12@users.noreply.github.com>
2021-05-05 00:35:25 +0900

    210505

---
+++ Programmers/Sort42748.cpp
@@ -0,0 +1,26 @@
+// https://programmers.co.kr/learn/courses/30/lessons/42748
+
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> solution(vector<int> array, vector<vector<int>> commands) {
+    vector<int> answer;
+    
+    for(int i = 0; i < commands.size(); i++){
+        vector<int> temp;
+        
+        for(int j = commands[i][0]-1; j < commands[i][1]; j++){
+            temp.push_back(array[j]);
+        }
+        
+        sort(temp.begin(), temp.end());
+        
+        answer.push_back(temp[commands[i][2]-1]);
+        temp.clear();
+    }
+    
+    return answer;
+}
