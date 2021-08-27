6c707ad1126114955bc2483d5b7c9534318aa5c2
minie12 <30361753+minie12@users.noreply.github.com>
2021-08-27 19:21:49 +0900

    210827

---
+++ leetcode/leet1791.cpp
@@ -0,0 +1,12 @@
+// https://leetcode.com/problems/find-center-of-star-graph/submissions/
+
+class Solution {
+public:
+    int findCenter(vector<vector<int>>& edges) {
+        int first = edges[0][0];
+        int second = edges[0][1];
+        
+        if(first == edges[1][0] || first == edges[1][1]) return first;
+        else return second;
+    }
+};
