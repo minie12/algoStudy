1d4477483b6bf567287b9cdf696cb7d8908f9d76
Eunjung Lim <30361753+minie12@users.noreply.github.com>
2022-01-20 00:20:31 +0900

    220120

---
+++ 이것이취업을위한코딩테스트/Q02.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+	string N;
+	cin >> N;
+	
+	int ans = 0;
+	
+	for(int i = 0; i < N.length(); i++){
+		int num = N[i] - 48;
+		if(num == 0 || num == 1 || ans == 0) ans += num;
+		else ans *= num;
+	}
+	
+	cout << ans;
+	
+	
+	return 0;
+}
