73941c3604d5375b1522e12ae2e595595980b433
Eunjung Lim <30361753+minie12@users.noreply.github.com>
2022-01-30 20:35:07 +0900

    Create Q07.cpp

---
+++ 이것이취업을위한코딩테스트/Q07.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+	string N;
+	cin >> N;
+	
+	int left = 0;int right = 0;
+	int mid = N.length()/2;
+	for(int i = 0; i < N.length(); i++){
+		if(i >= mid) right += N[i];
+		else left += N[i];
+	}
+	
+	if(right == left) cout << "LUCKY";
+	else cout << "READY";
+	
+	return 0;
+}
